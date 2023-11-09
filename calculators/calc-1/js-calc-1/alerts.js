62c074ecf33d57638f8f1c6136d6efa04ec45945
nstratiev <n.stratiev@gmail.com>
2023-11-10 01:33:44 +0200

    update

---
+++ calculators/calc-1/js-calc-1/alerts.js
@@ -0,0 +1,34 @@
+// --- Alert functions
+export function temporaryOnClickAlert(msg, duration, bgColor) {
+  const containerEl = document.createElement('div');
+  containerEl.setAttribute('class', 'temp-alert');
+  containerEl.style.backgroundColor = bgColor;
+  containerEl.style.outline = `2px solid ${bgColor}`;
+
+  const spanEl = document.createElement('span');
+  spanEl.setAttribute('class', 'check-mark');
+  spanEl.innerHTML = msg;
+  containerEl.appendChild(spanEl);
+  setTimeout(function () {
+    containerEl.parentNode.removeChild(containerEl);
+  }, duration);
+  document.body.appendChild(containerEl);
+}
+
+export const resetConfirmDialog = new ConfirmModal({
+  titleText: '',
+  msgText:
+    'Желаете ли всички запаметени стойности, свързани с настоящата страница, да бъдат изтрити?',
+  confirmText: 'Да',
+  cancelText: 'Не',
+});
+
+export const alertEmptyFieldBox = new ConfirmModal({
+  titleText: '',
+  msgText: 'Необходимо е всички полета да бъдат попълнени ...',
+  confirmText: 'OK',
+  cancelText: '',
+});
+
+// IMPORTS
+import { ConfirmModal } from '../../common-js/modalClass.js';
---
+++ calculators/calc-1/js-calc-1/reset.js
@@ -0,0 +1,52 @@
+// Reset functions
+export function resetMainForm() {
+  resetMainInputs();
+  resetMainPrimaryResults();
+  resetMainSecondaryResults();
+
+  setTimeout(() => {
+    resetConfirmDialog
+      .open()
+      .then((val) => {
+        localStorage.clear();
+        temporaryOnClickAlert('&check;', 500, 'rgb(192, 0, 0)');
+      })
+      .catch((val) => {});
+  }, 300);
+}
+
+export function resetMainInputs() {
+  numberFieldsMain.forEach((inp) => {
+    inp.value = '';
+  });
+}
+
+export function resetMainPrimaryResults() {
+  doughWeightElement.textContent = '####';
+  flourWeightElement.textContent = '####';
+  leavenWeightElement.textContent = '####';
+  waterWeightElement.textContent = '####';
+  saltWeightElement.textContent = '####';
+}
+
+export function resetMainSecondaryResults() {
+  flourLeavenElement.textContent = '###';
+  waterLeavenElement.textContent = '###';
+  flourTotalElement.textContent = '###';
+  waterTotalElement.textContent = '###';
+}
+
+// IMPORTS
+import { resetConfirmDialog, temporaryOnClickAlert } from './alerts.js';
+import {
+  numberFieldsMain,
+  doughWeightElement,
+  flourWeightElement,
+  waterWeightElement,
+  saltWeightElement,
+  leavenWeightElement,
+  flourLeavenElement,
+  waterLeavenElement,
+  flourTotalElement,
+  waterTotalElement,
+} from './elements.js';
