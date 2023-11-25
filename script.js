69875f8431a0af7a7627eae6a4ef4033147e3785
nstratiev <n.stratiev@gmail.com>
2023-11-25 17:06:01 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const formElem = document.querySelector('form');
 const btnClickElem = document.querySelector('.btn-click');
 const textField = document.querySelector('.text-field');
+const outputElem = document.querySelector('.output');
 
 formElem.addEventListener('keydown', (ev) => {
   if (ev.target.tagName === 'INPUT' && ev.key === 'Enter') {
@@ -14,8 +15,13 @@ btnClickElem.addEventListener('click', onClick);
 function onClick(ev) {
   const val = textField.value;
   const valAsNum = Number(val);
+  textField.value = '';
 
-  console.log(valAsNum);
-  console.log(typeof valAsNum);
-  console.log(Boolean(valAsNum));
+  outputElem.innerHTML += `${valAsNum}<br>${typeof valAsNum}<br>${Boolean(
+    valAsNum
+  )}`;
+  outputElem.innerHTML += '<br>' + '*****' + '<br>';
+  // console.log(valAsNum);
+  // console.log(typeof valAsNum);
+  // console.log(Boolean(valAsNum));
 }
