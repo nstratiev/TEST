22c3e58cbb364f621b733d6b9514336b2d68b586
nstratiev <n.stratiev@gmail.com>
2024-06-06 17:15:46 +0300

    a

--- calendar/js/date.js
+++
@@ -1,9 +0,0 @@
-// Date variables
-export const d = new Date();
-export const currYear = d.getFullYear();
-export const currMonthIndex = d.getMonth();
-export const currMonthEngName = monthsArr[currMonthIndex];
-export const currDate = d.getDate();
-
-// Imports
-import { monthsArr } from './constants.js';
