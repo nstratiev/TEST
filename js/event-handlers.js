b53ed033182389528c61fd5d10201ffce80fdf05
nstratiev <n.stratiev@gmail.com>
2024-07-13 21:37:02 +0300

    u

---
+++ js/event-handlers.js
@@ -0,0 +1,70 @@
+export function onSubmit_initialLoad(ev) {
+  onSubmit(ev, formMain, { isFirstPageLoad: true }, printResult_formMain);
+}
+
+export function onSubmit_fMain(ev) {
+  onSubmit(ev, formMain, { isFirstPageLoad: false }, printResult_formMain, resetResults_fMain, '#results-fMain');
+}
+
+export function onSave_fMain(ev) {
+  onSave(ev, formMain, true);
+}
+
+// Basic
+export function onGlobalSave(ev) {
+  const allForms = document.querySelectorAll('form');
+
+  for (const form of allForms) {
+    onSave(ev, form, false);
+  }
+
+  checkmarkAlertGreen(true);
+}
+
+function onSave(ev, formElem, hasCheckmark) {
+  const currFormData = getFormDataToObject(formElem);
+  const currFormName = formElem.name;
+  setLocalStorage(currFormData, currFormName);
+
+  checkmarkAlertGreen(hasCheckmark);
+}
+
+function onSubmit(ev, formElem, loadingConfig, printFunc, resetFormResultsFunc, href) {
+  if (loadingConfig.isFirstPageLoad !== true) {
+    ev.preventDefault();
+  }
+
+  const data = getData(formElem, loadingConfig, resetFormResultsFunc);
+
+  if (data === false) {
+    return;
+  }
+
+  setLocalStorage(data, formElem.name);
+  const resultsData = calculate_fMain(data);
+  printFunc(resultsData);
+
+  if (loadingConfig.isFirstPageLoad !== true) {
+    checkmarkAlertGreen(true);
+
+    const screenWidth = window.screen.width;
+    const screenAvailWidth = window.screen.availWidth;
+
+    if (href) {
+      if (screenWidth <= 680 || screenAvailWidth <= 680) {
+        location.href = href;
+      }
+
+    }
+  }
+
+}
+
+
+import { printResult_formMain } from './print.js';
+import { formMain } from './elements.js';
+import { getData, getFormDataToObject } from './generic.js';
+import { setLocalStorage } from './storage.js';
+import { resetResults_fMain } from './reset.js';
+import { checkmarkAlertGreen } from './alerts.js';
+import { calculate_fMain } from './math.js';
---
+++ js/index.js
@@ -0,0 +1,50 @@
+// On first page load
+document.addEventListener('DOMContentLoaded', (ev) => {
+  populateLocaleStorageData([formMain]);
+  onSubmit_initialLoad(ev);
+});
+
+// -- Button-to-top listeners
+window.addEventListener('scroll', onScreenScroll);
+btnToTop.addEventListener('click', goToScreenTop);
+
+// -- Buttons - global
+btnGlobalReset.addEventListener('click', onGlobalReset);
+btnGlobalSave.addEventListener('click', onGlobalSave);
+
+// Listeners
+attachFocusoutValidation();
+attachOnFocusoutDecimalPointValidation();
+
+// -- Buttons listeners
+document.addEventListener('click', (ev) => {
+  if (ev.target.tagName !== 'BUTTON') {
+    return;
+  }
+
+  const btnsGroupClassName = ev.target.parentElement.className;
+  const btnClassName = ev.target.className;
+
+  if (btnsGroupClassName.includes('-main')) {
+    // console.log('MAIN');
+    if (btnClassName === 'btn-submit') {
+      onSubmit_fMain(ev);
+
+    } else if (btnClassName === 'btn-reset') {
+      onReset_fMain(true);
+
+    } else if (btnClassName === 'btn-save') {
+      onSave_fMain(ev);
+    }
+  }
+
+});
+
+
+import { btnGlobalReset, btnGlobalSave, btnToTop, formMain } from './elements.js';
+import { onGlobalSave, onSave_fMain, onSubmit_fMain, onSubmit_initialLoad } from './event-handlers.js';
+import { attachFocusoutValidation } from './focusout.js';
+import { populateLocaleStorageData } from './storage.js';
+import { onGlobalReset, onReset_fMain } from './reset.js';
+import { goToScreenTop, onScreenScroll } from "./scroll.js";
+import { attachOnFocusoutDecimalPointValidation } from './validation.js';
